c05662792c876e36c1a17bd1eb72834c16605f27
Volkan Özçelik <me@volkan.io>
2026-01-05 04:08:50 -0800

    keep both fields.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- cloudflare.sh
+++ cloudflare.sh
@@ -19,8 +19,19 @@ DIR="${1:-.}"
 
 # Find all .html files (excluding already doubled ones)
 find "$DIR" -type f -name "*.html" ! -name "*.html.html" | while read -r file; do
-    mv "$file" "${file}.html"
+    cp "$file" "${file}.html"
     echo "Renamed: $file → ${file}.html"
 done
+
+echo "Done!"
+
+## Find all .html files, restructure into directories
+# This will not work because of internal links.
+#find "$DIR" -type f -name "*.html" ! -name "index.html" | while read -r file; do
+#    dir="${file%.html}"
+#    mkdir -p "$dir"
+#    mv "$file" "$dir/index.html"
+#    echo "Restructured: $file → $dir/index.html"
+#done
 
 echo "Done!"
